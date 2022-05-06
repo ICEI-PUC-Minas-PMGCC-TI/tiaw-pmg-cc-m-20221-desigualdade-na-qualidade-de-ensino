e6957ae3706ee49f71d8324d5585465b0f305ce7
Nilson Deon <NilsonDeon01@gmail.com>
2022-05-06 18:31:30 -0300

    create_registro

--- codigo/nilson/script.js
+++ codigo/nilson/script.js
@@ -1,10 +1,12 @@
-var visibilidade = true; //Variável que vai manipular o botão Exibir/ocultar
 
-function exibir() {
+/* REGISTRO INICIO */
+
+function exibir(){
     document.getElementById("formulario2").style.display = "block";
 }
 
-function ocultar() {
+function ocultar(){
     document.getElementById("formulario1").style.display = "none";
 }
 
+/* REGISTRO FIM */
